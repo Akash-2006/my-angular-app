d6d72fa6c6bfb4a7e8680505439345d9f4ba1ff4
Akash-2006 <freeshu966@gmail.com>
2025-06-11 15:35:38 +0530

    added feature to add a task

--- src/app/tasks/new-task-bar/new-task-bar.component.ts
+++ src/app/tasks/new-task-bar/new-task-bar.component.ts
@@ -1,16 +1,32 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+export interface newTaskType {
+  taskTiltle: string;
+  taskSummary: string;
+  dueDate: string;
+}
 @Component({
   selector: 'app-new-task-bar',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task-bar.component.html',
   styleUrl: './new-task-bar.component.css',
 })
 export class NewTaskBarComponent {
   @Output() close = new EventEmitter<void>();
+  @Output() submit = new EventEmitter<newTaskType>();
+  taskTitle = '';
+  taskSummary = '';
+  dueDate = '';
 
   onClose() {
     this.close.emit();
+  }
+  onSubmit() {
+    this.submit.emit({
+      taskTiltle: this.taskTitle,
+      taskSummary: this.taskSummary,
+      dueDate: this.dueDate,
+    });
   }
 }
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -9,7 +9,10 @@ import {
 import { userType } from '../app.component';
 import { TaskComponent } from './task/task.component';
 import { dummyTasks } from '../dummy-tasks';
-import { NewTaskBarComponent } from './new-task-bar/new-task-bar.component';
+import {
+  NewTaskBarComponent,
+  newTaskType,
+} from './new-task-bar/new-task-bar.component';
 export interface TaskType {
   id: string;
   userId: string;
@@ -30,6 +33,7 @@ export class TasksComponent {
   @Input({ required: true }) user?: Signal<userType>;
   tasksOrigin: WritableSignal<TaskType[]> = signal(dummyTasks);
   isNewTaskSelected = false;
+
   tasks = computed(() =>
     this.tasksOrigin().filter((task) => this.user?.()?.id === task.userId)
   );
@@ -43,6 +47,19 @@ export class TasksComponent {
     this.isNewTaskSelected = true;
   }
   onSelectClose() {
+    this.isNewTaskSelected = false;
+  }
+  addTaskToUser(event: newTaskType) {
+    const newTask: TaskType = {
+      id: crypto.randomUUID(),
+      userId: this.user?.()?.id || '',
+      title: event.taskTiltle,
+      dueDate: event.dueDate,
+      summary: event.taskSummary,
+      isDone: false,
+    };
+    this.tasksOrigin.set([...this.tasksOrigin(), newTask]);
+    console.log('New task added:', this.tasksOrigin());
     this.isNewTaskSelected = false;
   }
 }
